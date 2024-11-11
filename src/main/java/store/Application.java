337a04540df916f75433129919a12ce5c828166b
sejinezy <psj8880@naver.com>
2024-11-11 20:43:08 +0900

    feat(Application): 클래스 구현
    - main() 메서드를 포함한 애플리케이션 진입점 구현.
    - 사용자와의 상호작용 루프 추가.

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,7 +1,26 @@
 package store;
 
+import store.config.Config;
+import store.controller.PurchaseController;
+import store.view.InputView;
+import store.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Config config = new Config();
+        OutputView outputView = config.getOutputView();
+        PurchaseController controller = config.getPurchaseController();
+
+        outputView.printWelcome();
+        boolean continueShopping = true;
+
+        while (continueShopping) {
+            try {
+                controller.processShopping();
+                continueShopping = controller.askContinueShopping();
+            } catch (IllegalArgumentException e) {
+                outputView.printError(e.getMessage());
+            }
+        }
     }
 }
