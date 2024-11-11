6e2997f2b0d4df1e7865cef41e8ba1879488065a
sejinezy <psj8880@naver.com>
2024-11-11 20:13:35 +0900

    test(StockTest): 테스트 추가

--- src/test/java/store/domain/product/StockTest.java
+++ src/test/java/store/domain/product/StockTest.java
@@ -15,5 +15,26 @@ class StockTest {
     void 재고가_음수일_때_오류발생() {
         assertThrows(IllegalArgumentException.class, () -> new Stock(-10, 20));
     }
+
+    @Test
+    void 재고_감소_성공() {
+        Stock stock = new Stock(20, 10);
+
+        stock.decreaseNormalStock(5);
+        stock.decreasePromotionStock(3);
+
+        assertEquals(15, stock.getNormalStock());
+        assertEquals(7, stock.getPromotionStock());
+    }
+
+    @Test
+    void 재고_감소_초과_오류발생() {
+        Stock stock = new Stock(20, 10);
+
+        assertThrows(IllegalArgumentException.class, () -> stock.decreaseNormalStock(25));
+        assertThrows(IllegalArgumentException.class, () -> stock.decreasePromotionStock(15));
+    }
+
+
 }
 
