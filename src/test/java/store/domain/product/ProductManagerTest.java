a57f81827e845a2cb1e58dec91b689641733a0cc
sejinezy <psj8880@naver.com>
2024-11-11 20:50:42 +0900

    test(ProductManagerTest): 테스트 추가

--- src/test/java/store/domain/product/ProductManagerTest.java
+++ src/test/java/store/domain/product/ProductManagerTest.java
@@ -23,5 +23,29 @@ class ProductManagerTest {
 
         assertNotNull(manager);
     }
+
+    @Test
+    void 상품_검색_성공() {
+        List<ProductInfo> productInfos = List.of(
+                new ProductInfo("Cola", 1500, 10, "탄산2+1")
+        );
+        ProductManager manager = new ProductManager(productInfos, List.of());
+
+        ProductInventory product = manager.findProduct("Cola");
+
+        assertEquals("Cola", product.getName());
+    }
+
+    @Test
+    void 존재하지_않는_상품_검색_오류() {
+        List<ProductInfo> productInfos = List.of(
+                new ProductInfo("Cola", 1500, 10, "탄산2+1")
+        );
+        ProductManager manager = new ProductManager(productInfos, List.of());
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> manager.findProduct("Unknown"));
+        assertEquals("[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요.", exception.getMessage());
+    }
 }
 
